3da77abbb1a9854358fe53182387427ead466c2f
noahbachmann <trainpaths@gmail.com>
2026-05-15 10:27:47 +0200

    feat: add widthHeightRatio

--- geometry.ts
+++ geometry.ts
@@ -1,29 +1,35 @@
-import { Rectangular } from "./geometry_third_party.ts";
+import { Rectangle, Rectangular } from "./geometry_third_party.ts";
 
 export interface Quadratic {
-  getSide(): number;
+	getSide(): number;
 }
 
-export class Square implements Quadratic {
-  constructor(private side: number) {}
+export class Square extends Rectangle implements Quadratic {
+	constructor(
+		width: number,
+		height: number,
+		private side: number,
+	) {
+		super(width, height);
+	}
 
-  public toString(): string {
-    return `Square(${this.side})`;
-  }
+	public toString(): string {
+		return `Square(${this.side})`;
+	}
 
-  public getSide(): number {
-    return this.side;
-  }
+	public getSide(): number {
+		return this.side;
+	}
 }
 
 export class RectangleAdapter implements Rectangular {
-  constructor(private square: Quadratic) {}
+	constructor(private square: Quadratic) {}
 
-  public getWidth(): number {
-    return this.square.getSide();
-  }
+	public getWidth(): number {
+		return this.square.getSide();
+	}
 
-  public getHeight(): number {
-    return this.square.getSide();
-  }
+	public getHeight(): number {
+		return this.square.getSide();
+	}
 }
